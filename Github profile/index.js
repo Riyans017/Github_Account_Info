2cc7f3e785233e126beed9039445d42894115da9
Riyans017 <riyans00007@gmail.com>
2024-09-09 21:30:30 +0545

    Added

--- Github profile/index.js
+++ Github profile/index.js
@@ -9,24 +9,36 @@ let search = async()=>{
 }
 
 let values = async(val)=>{
+    
     let url = `https://api.github.com/users/${val}`;
-
     try{
-       let res = await fetch(url);
-       if(res.ok){
-         let main = await res.json();
-       
-
-         show_data.style.display = 'block';
-          let img = document.getElementsByClassName('img')[0];
-         img.innerHTML = `<img src=${main.avatar_url} />`
-         p.style.display = 'block';
-           let details = document.getElementsByClassName('personal_info')[0];
-           details.innerHTML = "Name : " +  main.login + "<br>" + "Id : " +  main.id + "<br>" + "Email : "+ main.email + "<br>" +"Node_id : " +  main.node_id + "<br>";
-       }
-       else{
-        alert("NO such data is present")
-       }
+        if(val === ""){
+            show_data.style.display = 'block';
+            show_data.style.fontSize = "2.4rem"
+            show_data.style.textAlign = 'center'
+             show_data.innerHTML = "Please Enter Any Data"
+        }
+        else if(val.length >1){
+            let res = await fetch(url);
+            if(res.ok){
+              let main = await res.json();
+            
+     
+              show_data.style.display = 'block';
+               let img = document.getElementsByClassName('img')[0];
+              img.innerHTML = `<img src=${main.avatar_url} />`
+              p.style.display = 'block';
+                let details = document.getElementsByClassName('personal_info')[0];
+                details.innerHTML = "Name : " +  main.login + "<br>" + "Id : " +  main.id + "<br>" + "Email : "+ main.email + "<br>" +"Node_id : " +  main.node_id + "<br>";
+            }
+            else{
+                show_data.style.display = 'block';
+                show_data.style.textAlign = 'center'
+                show_data.style.fontSize = "2.4rem"
+                 show_data.innerHTML = "No Such User is Available"
+               }
+        }
+      
     }
     catch(err){
         console.log("the error is",err);
